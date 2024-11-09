2343c6141093c8a0e90ddb271a357c82ea9ef1db
Apollo-Blaze <srichandsureshrocks@gmail.com>
2024-11-09 21:20:43 +0530

    front deployment

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import IntroPage from './components/Intro';
 import EndPage from './components/EndPage';
 import Home from './components/Home'; // If you have this as the main page
-import { useState } from 'react';
 import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
-  const [killerGuess, setKillerGuess] = useState('');
-
-  const handleSubmitGuess = (guess) => {
-    // Assume 'Alice' is the correct killer
-    const correctKiller = 'Alice';
-    const isCorrect = guess === correctKiller;
-
-    // Navigate to EndPage with the result and guessed killer
-    navigate('/end', { state: { result: isCorrect, killer: correctKiller } });
-  };
-
   return (
     <Router>
        <Analytics/>
