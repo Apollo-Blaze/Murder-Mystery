730f51676eb8d31a1770512c42430ac247a4a9f4
Apollo-Blaze <srichandsureshrocks@gmail.com>
2024-11-20 19:40:32 +0530

    Added analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React,{ Suspense } from 'react';
 import './Loader.css'; // Import the CSS file for styling the loader
 import './App.css'
+import {Analytics} from '@vercel/analytics/react'
 
 const Loader = () => (
   <div className="loader-container">
@@ -24,6 +25,7 @@ const App = () => {
         v7_relativeSplatPath: true,  // Enable relative splat path resolution
       }}
     >
+      <Analytics />
       <Suspense fallback={<Loader />}>
         <Routes>
           <Route path="/" element={<IntroPage />} />
